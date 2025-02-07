d1c6c2cb4a7cad975c5ee385c39bbc039f4d6025
luigisaetta <luigi.saetta@gmail.com>
2025-02-07 17:28:17 +0100

    some refactoring

--- oci_models.py
+++ oci_models.py
@@ -8,8 +8,9 @@ from config_private import COMPARTMENT_OCID
 
 config_reader = ConfigReader("config.toml")
 
-MODEL_ID = config_reader.find_key("router_model_id")
-ENDPOINT = config_reader.find_key("router_model_endpoint")
+# we're using command-r-plus model for routing
+ROUTER_MODEL_ID = config_reader.find_key("router_model_id")
+ROUTER_ENDPOINT = config_reader.find_key("router_model_endpoint")
 
 
 def create_model_for_routing():
@@ -17,9 +18,11 @@ def create_model_for_routing():
     Create the OCI Model for routing
     """
     llm = ChatOCIGenAI(
-        model_id=MODEL_ID,
+        model_id=ROUTER_MODEL_ID,
         compartment_id=COMPARTMENT_OCID,
-        service_endpoint=ENDPOINT,
+        service_endpoint=ROUTER_ENDPOINT,
+        # for the router we need deterministic output (temp=0)
+        # and we don't need many tokens for output (max_tokens=512)
         model_kwargs={"temperature": 0, "max_tokens": 512},
     )
     return llm
