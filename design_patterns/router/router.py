ac574a327fc6acb41e3c14bd0630fda73d01b518
luigisaetta <luigi.saetta@gmail.com>
2025-03-03 09:26:53 +0100

    added router

---
+++ design_patterns/router/router.py
@@ -0,0 +1,21 @@
+"""
+Router implementation based on llm_structured
+"""
+
+from structured_llm import StructuredLLM
+
+
+class Router(StructuredLLM):
+    """
+    This class implement a rtouter using Llama 3.3, using the example StructuredLLM
+    """
+
+    def route(self, state: type) -> dict:
+        """
+        Route the user input to the correct endpoint
+
+        state: the state of the workflow
+        """
+        new_state = self.invoke(state.input)
+
+        return new_state
